10e303f078b7290d53d71781ff15a933e34ab6e5
Irfan <imak_7@yahoo.com>
2022-05-06 13:08:17 -0500

    change var to let

--- src/controllers/github.js
+++ src/controllers/github.js
@@ -3,7 +3,7 @@ import { Error, Response, Info } from "../models/error"
 
 
 export async function getGithubRepoPullRequestData(req, res = undefined) {
-    var response = new Response('', new Error('Ok', 0));
+    let response = new Response('', new Error('Ok', 0));
     let array = new Array();
     let ownerName = '';
     let repoName = '';
