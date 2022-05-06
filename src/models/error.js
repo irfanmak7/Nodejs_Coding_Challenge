0f6d58ffa99e7bf23975f7b2722b739567877548
Irfan <imak_7@yahoo.com>
2022-05-06 13:00:25 -0500

    create Info class

--- src/models/error.js
+++ src/models/error.js
@@ -4,3 +4,25 @@ export class Error {
         this.code = code;
     }
 }
+
+export class Info {
+    constructor(result = undefined) {
+        if (result === undefined) {
+            this.foundCount = 0;
+            this.tableCount = 0;
+        } else {
+            try {
+                this.foundCount = result['foundCount'];
+            } catch(error) {
+                this.foundCount = 0;
+                this.foundCountError = new Error(error, 500);
+            }
+            try {
+                this.tableCount = result['tableCount'];
+            } catch(error) {
+                this.tableCount = 0;
+                this.tableCountError = new Error(error, 500);
+            }
+        }
+    }
+}
