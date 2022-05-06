d1ccc6f59e6e1de2d871e839a01870be4a7a2ee4
Irfan <imak_7@yahoo.com>
2022-05-06 13:08:02 -0500

    set initial variables

--- src/controllers/github.js
+++ src/controllers/github.js
@@ -3,5 +3,8 @@ import { Error, Response, Info } from "../models/error"
 
 
 export async function getGithubRepoPullRequestData(req, res = undefined) {
-
+    var response = new Response('', new Error('Ok', 0));
+    let array = new Array();
+    let ownerName = '';
+    let repoName = '';
 }
